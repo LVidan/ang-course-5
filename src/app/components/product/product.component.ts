fdfa67491f7b934cc612f3f25e88bfe595bf07df
LVidan <vidan.lugonja@gmail.com>
2022-05-03 13:03:50 +0200

    DEV-(03.05): add products table

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -10,6 +10,19 @@ export class ProductComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.products);
+
   }
 
+  tableHeadings: string[] = ["ID", "NAME", "PRICE", "LOCATION", "PRICE DESC."]
+  // products: any[] = [{ "Name": "Cheese", "Price": 2.50, "Location": "Refrigerated foods", "Type": "C" }, { "Name": "Crisps", "Price": 3, "Location": "the Snack isle", "Type": "M" }, { "Name": "Pizza", "Price": 4, "Location": "Refrigerated foods", "Type": "?" }, { "Name": "Chocolate", "Price": 1.50, "Location": "the Snack isle", "Type": "C" }, { "Name": "Caviar", "Price": 45.50, "Location": "Refrigerated foods", "Type": "E" }, { "Name": "Self-raising flour", "Price": 1.50, "Location": "Home baking", "Type": "C" }, { "Name": "Ground almonds", "Price": 3, "Location": "Home baking", "Type": "M" }]
+  products: any = [
+    { "Name": "Cheese", "Price": 2.50, "Location": "Refrigerated foods", "Type": "C" },
+    { "Name": "Crisps", "Price": 3, "Location": "the Snack isle", "Type": "M" },
+    { "Name": "Pizza", "Price": 4, "Location": "Refrigerated foods", "Type": "?" },
+    { "Name": "Chocolate", "Price": 1.50, "Location": "the Snack isle", "Type": "C" },
+    { "Name": "Caviar", "Price": 45.50, "Location": "Refrigerated foods", "Type": "E" },
+    { "Name": "Self-raising flour", "Price": 1.50, "Location": "Home baking", "Type": "C" },
+    { "Name": "Ground almonds", "Price": 3, "Location": "Home baking", "Type": "M" }
+  ];
 }
